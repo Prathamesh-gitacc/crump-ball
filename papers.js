b6fe5665b659c44b082f8207bce65f1bc850f27b
Prathamesh-gitacc <76098843+Prathamesh-gitacc@users.noreply.github.com>
2021-01-27 19:11:28 +0530

    Add files via upload

---
+++ papers.js
@@ -0,0 +1,15 @@
+class paper{
+    constructor(x, y) {
+        var options = {
+            isStatic:false,
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+
+    }
+
+    display(){
+   super.display();
+    }
+}
